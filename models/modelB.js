13ff659b4465cf6981848508c572836941315857
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-07-22 14:56:13 -0400

    Add vehicle model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var submarine = {
+  name: "Nautilus",
+  captain: "Nemo",
+  numCrewMembers: 45,
+  numGuests: 3,
+  isLegendary: true,
+  isSeaMonster: false,
+  guestNames: ["Pierre Aronnax", "Conseil", "Ned Land"],
+  visitedDestinations: ["Antarctica", "Spain", "Atlantic Sea Bottom", "Atlantis (ruins)"]
+}
