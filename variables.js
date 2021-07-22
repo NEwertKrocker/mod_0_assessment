8ec518ef03cb0186ada7d07a04153c4bed25e29c
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-07-22 14:44:17 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = "Sam Neill";
+var isActive = true;
+famousPerson = "Jeff Goldblum";
+var num3 = 51;
+var num9 = 255;
+var sum = num3 + num9;
+var animals = ["Hippogriff", "Bandersnatch", "Sasquatch"];
+animals.shift();
+console.log(famousPerson.length);
