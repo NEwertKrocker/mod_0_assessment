37bc849c0b5bfb26a7828e5142c7e99e3964d821
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-07-22 14:50:02 -0400

    Add airport model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: "Cleveland Hopkins International Airport",
+  iataCode: "CLE",
+  yearlyPassengers: 4122517,
+  runways: 3,
+  isOpen: true,
+  isCursed: true,
+  topAirlines: ["Frontier", "Spirit", "Southwest", "United", "American"],
+  topDestinations: ["Orlando", "Atlanta", "Denver", "Charlotte", "Chicago"]
+}
